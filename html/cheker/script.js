e70ad693549c9c180d581393911d6d09f929b654
Ummida Maden <abdurahim.amirovv@gmail.com>
2024-03-16 16:04:16 +0600

    images

--- html/cheker/script.js
+++ html/cheker/script.js
@@ -1,9 +1,8 @@
-'use strict';
+"use strict";
 
+const preloader = document.querySelector("[data-preaload]");
 
-const preloader = document.querySelector("[data-preaload]")
-
-window.addEventListener("load", function () {
-    preloader.classList.add("loaded");
-    document.body.classList.add("loaded")
-})
+window.addEventListener("load", function (preloader) {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
